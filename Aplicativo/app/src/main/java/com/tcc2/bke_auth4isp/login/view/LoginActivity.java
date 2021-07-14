b404ee791491c5656c1f7e6816d7bd5b48afa167
anonymousgithub21 <anonymousgithub21@gmail.com>
2021-07-14 10:39:30 -0300

    CPF mask

--- Aplicativo/app/src/main/java/com/tcc2/bke_auth4isp/login/view/LoginActivity.java
+++ Aplicativo/app/src/main/java/com/tcc2/bke_auth4isp/login/view/LoginActivity.java
@@ -2,6 +2,7 @@ package com.tcc2.bke_auth4isp.login.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.MaskFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,14 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.tcc2.bke_auth4isp.R;
+import com.tcc2.bke_auth4isp.common.CpfMask;
 import com.tcc2.bke_auth4isp.dialogs.SelectionIspDialog;
 import com.tcc2.bke_auth4isp.entity.ISP;
 import com.tcc2.bke_auth4isp.login.LoginContracts;
 import com.tcc2.bke_auth4isp.login.presenter.LoginPresenter;
 import com.tcc2.bke_auth4isp.login.router.LoginRouter;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity implements LoginContracts.View {
@@ -34,6 +37,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContracts.V
         presenter = new LoginPresenter(this, router);
         activity = this;
         username = findViewById(R.id.username);
+        username.addTextChangedListener(CpfMask.insert(username));
         password = findViewById(R.id.password);
         buttonConfirmLogin = findViewById(R.id.buttonLogin);
         buttonConfirmLogin.setOnClickListener(new View.OnClickListener() {
